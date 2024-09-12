19b78c87fafe89cf77281e4113d1cade491935d0
SACHINMUSIC <sachinxpapa@gmail.com>
2024-09-12 23:32:38 +0530

    ❖ | ᴀʟᴇxᴀ ꭙ ᴍᴜsɪᴄ | ❖

--- NEXIOMUSIC/plugins/bot/start.py
+++ NEXIOMUSIC/plugins/bot/start.py
@@ -1,23 +1,19 @@
-import asyncio
-import random
 import time
+import random
 from pyrogram import filters
 from pyrogram.enums import ChatType
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto, Message
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from youtubesearchpython.__future__ import VideosSearch
 
 import config
 from NEXIOMUSIC import app
 from NEXIOMUSIC.misc import _boot_
 from NEXIOMUSIC.plugins.sudo.sudoers import sudoers_list
-from NEXIOMUSIC.utils import bot_sys_stats
 from NEXIOMUSIC.utils.database import (
     add_served_chat,
     add_served_user,
     blacklisted_chats,
     get_lang,
-    get_served_chats,
-    get_served_users,
     is_banned_user,
     is_on_off,
 )
@@ -60,9 +56,6 @@ HIMANSHI = [
 "CAACAgUAAxkBAAEBSCRm2-y-hL2Y3XHDu4Ykv_K8gh1AEgACQAUAAm6c-VWHUk18jds0QR4E",
 ]
 
-async def delete_sticker_after_delay(message, delay):
-    await asyncio.sleep(delay)
-    await message.delete()
 
 @app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
@@ -72,25 +65,22 @@ async def start_pm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            sticker_message = await message.reply_sticker(sticker=random.choice(STICKERS))
-            asyncio.create_task(delete_sticker_after_delay(sticker_message, 2))  # Delete sticker after 2 seconds
-            await message.reply_sticker(
-            random.choice(HIMANSHI),)
             return await message.reply_photo(
-            random.choice(NEXIO),
+                random.choice(NEXIO),
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
             )
-        elif name[0:3] == "sud":
+        if name[0:3] == "sud":
             await sudoers_list(client=client, message=message, _=_)
             if await is_on_off(2):
-                await app.send_message(
+                return await app.send_message(
                     chat_id=config.LOGGER_ID,
-                    text=f"❖ {message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>❍ sᴜᴅᴏʟɪsᴛ</b>.\n\n<b>❍ ᴜsᴇʀ ɪᴅ ➠</b> <code>{message.from_user.id}</code>\n<b>❖ ᴜsᴇʀɴᴀᴍᴇ ➠</b> @{message.from_user.username}",
+                    text=f"❖ {message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>sᴜᴅᴏʟɪsᴛ</b>.\n\n<b>❍ ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>❍ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
                 )
-        elif name[0:3] == "inf":
+            return
+        if name[0:3] == "inf":
             m = await message.reply_text("🔎")
-            query = str(name).replace("info_", "", 1)
+            query = (str(name)).replace("info_", "", 1)
             query = f"https://www.youtube.com/watch?v={query}"
             results = VideosSearch(query, limit=1)
             for result in (await results.next())["result"]:
@@ -114,36 +104,32 @@ async def start_pm(client, message: Message, _):
                 ]
             )
             await m.delete()
-            await app.send_video(
+            await app.send_photo(
                 chat_id=message.chat.id,
-                video=thumbnail,
+                photo=thumbnail,
                 caption=searched_text,
                 reply_markup=key,
             )
             if await is_on_off(2):
-                await app.send_message(
+                return await app.send_message(
                     chat_id=config.LOGGER_ID,
-                    text=f"❖ {message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>❍ ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>\n\n<b>❍ ᴜsᴇʀ ɪᴅ ➠</b> <code>{message.from_user.id}</code>\n<b>❍ ᴜsᴇʀɴᴀᴍᴇ ➠</b> @{message.from_user.username}",
+                    text=f"❖ {message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n<b>❍ ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>❍ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
                 )
     else:
         out = private_panel(_)
-        sticker_message = await message.reply_sticker(sticker=random.choice(STICKERS))
-        asyncio.create_task(delete_sticker_after_delay(sticker_message, 2))  # Delete sticker after 2 seconds
-        served_chats = len(await get_served_chats())
-        served_users = len(await get_served_users())
-        UP, CPU, RAM, DISK = await bot_sys_stats()
         await message.reply_sticker(
         random.choice(HIMANSHI),)
         return await message.reply_photo(
-        random.choice(NEXIO),
-            caption=_["start_2"].format(message.from_user.mention, app.mention, UP, DISK, CPU, RAM, served_users, served_chats),
+            random.choice(NEXIO),
+            caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
         )
         if await is_on_off(2):
-            await app.send_message(
+            return await app.send_message(
                 chat_id=config.LOGGER_ID,
-                text=f"❖ {message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n<b>❍ ᴜsᴇʀ ɪᴅ ➠</b> <code>{message.from_user.id}</code>\n<b>❖ ᴜsᴇʀɴᴀᴍᴇ ➠</b> @{message.from_user.username}",
+                text=f"❖ {message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n<b>❍ ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>❍ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
             )
+
 
 @app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)
 @LanguageStart
@@ -152,12 +138,13 @@ async def start_gp(client, message: Message, _):
     uptime = int(time.time() - _boot_)
     await message.reply_sticker(
         random.choice(HIMANSHI),)
-        return await message.reply_photo(
+    return await message.reply_photo(
         random.choice(NEXIO),
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
     )
     return await add_served_chat(message.chat.id)
+
 
 @app.on_message(filters.new_chat_members, group=-1)
 async def welcome(client, message: Message):
@@ -188,14 +175,13 @@ async def welcome(client, message: Message):
                 out = start_panel(_)
                 await message.reply_sticker(
                 random.choice(HIMANSHI),)
-                return await message.reply_photo(
-                random.choice(NEXIO),
-                caption=_["start_1"].
-                caption=_["start_3"].format(
-                message.from_user.mention,
-                app.mention,
-                message.chat.title,
-                app.mention,
+                await message.reply_photo(
+                    photo=config.START_IMG_URL,
+                    caption=_["start_3"].format(
+                        message.from_user.first_name,
+                        app.mention,
+                        message.chat.title,
+                        app.mention,
                     ),
                     reply_markup=InlineKeyboardMarkup(out),
                 )
