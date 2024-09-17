76fd8e9bffc419954d85f027ecec20b1bb27f98c
SACHINMUSIC <sachinxpapa@gmail.com>
2024-09-17 11:02:29 +0530

    [ ɴᴇxɪᴏ ᴍᴜsɪᴄ : 01 ]

--- NEXIOMUSIC/module/raid.py
+++ NEXIOMUSIC/module/raid.py
@@ -1,45 +1,54 @@
-import asyncio
-from random import choice
-from telethon import events
-from telethon import events, functions, types
-from NEXIOMUSIC import app
-from NEXIOMUSIC.misc import SUDOERS
-from NEXIOMUSIC.module.data import RAID
+import pyrogram
+import time
+import time
+from pyrogram import filters
+from pyrogram import Client
+from DAXXMUSIC import app
+from DAXXMUSIC.misc import SUDOERS
+#from NEXIOMUSIC.module.data import RAID
 
 OWNER_ID = "5959548791"
 SANATANI_TECH = "5959548791"
 
-@app.on_message(filters.command(["raid", "aid"], prefixes=["R", "/", "", "."]) & SUDOERS)
-async def raid(e):
-    if e.sender_id in SUDO_USERS:
-        xraid = e.text.split(" ", 2)
+RAID = [
+"𝗦𝗔𝗡𝗔𝗧𝗔𝗡𝗜 [ sᴀᴄʜɪɴ sᴀɴᴀᴛᴀɴɪ ] 𝗧𝗘𝗥𝗔 𝗕𝗔𝗔𝗣𝗣𝗣𝗣𝗣 𝗛𝗔𝗜 𝗥𝗔𝗡𝗗𝗜 𝗞𝗘",
+"ᴍᴀᴅᴀʀᴄʜᴏᴅ ᴛᴇʀɪ ᴍᴀᴀ ᴋɪ ᴄʜᴜᴛ ᴍᴇ ɢʜᴜᴛᴋᴀ ᴋʜᴀᴀᴋᴇ ᴛʜᴏᴏᴋ ᴅᴜɴɢᴀ 🤣🤣",
+"ᴛᴇʀᴇ ʙᴇʜᴇɴ ᴋ ᴄʜᴜᴛ ᴍᴇ ᴄʜᴀᴋᴜ ᴅᴀᴀʟ ᴋᴀʀ ᴄʜᴜᴛ ᴋᴀ ᴋʜᴏᴏɴ ᴋᴀʀ ᴅᴜɢᴀ",
+"ᴛᴇʀɪ ᴠᴀʜᴇᴇɴ ɴʜɪ ʜᴀɪ ᴋʏᴀ? 9 ᴍᴀʜɪɴᴇ ʀᴜᴋ ꜱᴀɢɪ ᴠᴀʜᴇᴇɴ ᴅᴇᴛᴀ ʜᴜ 🤣🤣🤩",
+"ᴛᴇʀɪ ᴍᴀᴀ ᴋ ʙʜᴏꜱᴅᴇ ᴍᴇ ᴀᴇʀᴏᴘʟᴀɴᴇᴘᴀʀᴋ ᴋᴀʀᴋᴇ ᴜᴅᴀᴀɴ ʙʜᴀʀ ᴅᴜɢᴀ ✈️🛫",
+"ᴛᴇʀɪ ᴍᴀᴀ ᴋɪ ᴄʜᴜᴛ ᴍᴇ ꜱᴜᴛʟɪ ʙᴏᴍʙ ꜰᴏᴅ ᴅᴜɴɢᴀ ᴛᴇʀɪ ᴍᴀᴀ ᴋɪ ᴊʜᴀᴀᴛᴇ ᴊᴀʟ ᴋᴇ ᴋʜᴀᴀᴋ ʜᴏ ᴊᴀʏᴇɢɪ💣",
+"ᴛᴇʀɪ ᴍᴀᴀᴋɪ ᴄʜᴜᴛ ᴍᴇ ꜱᴄᴏᴏᴛᴇʀ ᴅᴀᴀʟ ᴅᴜɢᴀ👅",
+]
 
-        if len(xraid) == 3:
-            entity = await e.client.get_entity(xraid[2])
-            uid = entity.id
+@app.on_message(filters.command("raid", prefixes=".") & SUDOERS)
+def spam_command(client, message):
+    try:
 
-        elif e.reply_to_msg_id:             
-            a = await e.get_reply_message()
-            entity = await e.client.get_entity(a.sender_id)
-            uid = entity.id
+        message.delete()
+    except pyrogram.errors.exceptions.FloodWait as e:
+        print(f"Error deleting message: {e}")
+        pass  
+        
+    if message.reply_to_message and message.reply_to_message.text:
+        user_to_tag = message.reply_to_message.from_user.mention()
+        command_args = message.text.split(".raid", 1)[-1].strip()
 
         try:
-            if uid in SANATANITECH:
-                await e.reply("❖ ɴᴏᴘᴇ, ᴛʜɪꜱ ɢᴜʏ ɪꜱ ᴀᴜᴛʜᴏʀ ʙᴏᴛ ᴏᴡɴᴇʀ ")
-            elif uid == OWNER_ID:
-                await e.reply("❖ ɴᴏᴘᴇ, ᴛʜɪꜱ ɢᴜʏ ɪꜱ ᴏᴡɴᴇʀ ᴏꜰ ᴛʜᴇꜱᴇ ʙᴏᴛꜱ")
-            elif uid in SUDO_USERS:
-                await e.reply("❖ ɴᴏᴘᴇ, ᴛʜɪꜱ ɢᴜʏ ɪꜱ ꜱᴜᴅᴏ ᴜꜱᴇʀ")
-            else:
-                first_name = entity.first_name
-                counter = int(xraid[1])
-                username = f"[{first_name}](tg://user?id={uid})"
-                for _ in range(counter):
-                    reply = choice(RAID)
-                    caption = f"{username} {reply}"
-                    await e.client.send_message(e.chat_id, caption)
-                    await asyncio.sleep(0.1)
-        except (IndexError, ValueError, NameError):
-            await e.reply(f"{hl}ʀᴀɪᴅ <ᴄᴏᴜɴᴛ> <ᴜꜱᴇʀɴᴀᴍᴇ ᴏꜰ ᴜꜱᴇʀ> <ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴜꜱᴇʀ>")
-        except Exception as e:
-            print(e)
+            num_times, text_to_spam = command_args.split(maxsplit=1)
+            num_times = int(num_times)
+        except ValueError:
+            num_times = 1
+            reply = random.choice(RAID)
+            text_to_spam = command_args
+
+        for _ in range(num_times):
+            message.reply_text(f"{user_to_tag} {reply}")
+            time.sleep(1) 
+    elif message.reply_to_message:
+        user_to_tag = message.reply_to_message.from_user.mention()
+
+        for _ in range(5):  
+            message.reply_to_message.reply_text(f"{user_to_tag} {reply}")
+            time.sleep(0.2)  
+    else:
+        message.reply_text("• AP ISKO USE NAHI KAR SAKTE SORRY")
