8ecbd9e3b3c6e5b729803f0e5b3d5a5dfc5e2780
SACHINMUSIC <sachinxpapa@gmail.com>
2024-09-17 20:01:53 +0530

    [ ɴᴇxɪᴏ ᴍᴜsɪᴄ : 01 ]

--- NEXIOMUSIC/plugins/bot/help.py
+++ NEXIOMUSIC/plugins/bot/help.py
@@ -88,29 +88,39 @@ async def helper_cb(client, CallbackQuery, _):
     elif cb == "hb15":
         await CallbackQuery.edit_message_text(helpers.HELP_15, reply_markup=keyboard)
 
-@app.on_callback_query(filters.regex("mbot_cb") & ~BANNED_USERS)
+@app.on_callback_query(filters.regex("NEXIO_CP") & ~BANNED_USERS)
 async def helper_cb(client, CallbackQuery):
     await CallbackQuery.edit_message_text(Helper.HELP_M, reply_markup=InlineKeyboardMarkup(BUTTONS.MBUTTON))
     
-@app.on_callback_query(filters.regex('managebot123'))
+@app.on_callback_query(filters.regex('NEXIO123'))
 async def on_back_button(client, CallbackQuery):
     callback_data = CallbackQuery.data.strip()
     cb = callback_data.split(None, 1)[1]
     keyboard = help_pannel(_, True)
-    if cb == "settings_back_helper":
+    if cb == "MAIN_HELP_CP":
         await CallbackQuery.edit_message_text(
             _["help_1"].format(SUPPORT_CHAT), reply_markup=keyboard
         )
+        
 
-@app.on_callback_query(filters.regex("bbot_cb") & ~BANNED_USERS)
+@app.on_callback_query(filters.regex("NEXIOO_CP") & ~BANNED_USERS)
 async def helper_cb(client, CallbackQuery):
     await CallbackQuery.edit_message_text(Helper.HELP_B, reply_markup=InlineKeyboardMarkup(BUTTONS.BBUTTON))
+    
+    
+    
+    
+@app.on_callback_query(filters.regex("MAIN_HELP_CP") & ~BANNED_USERS)
+async def helper_cb(client, CallbackQuery):
+    await CallbackQuery.edit_message_text(Helper.HELP_SACHIN, reply_markup=InlineKeyboardMarkup(BUTTONS.BBUTTON))
+        
 
-@app.on_callback_query(filters.regex('mplus'))      
+    
+@app.on_callback_query(filters.regex('NEXIOPLUS'))      
 async def mb_plugin_button(client, CallbackQuery):
     callback_data = CallbackQuery.data.strip()
     cb = callback_data.split(None, 1)[1]
-    keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("ʙᴀᴄᴋ", callback_data=f"mbot_cb")]])
+    keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("ʙᴀᴄᴋ", callback_data=f"MAIN_HELP_CP")]])
     if cb == "Okieeeeee":
         await CallbackQuery.edit_message_text(f"`something errors`",reply_markup=keyboard,parse_mode=enums.ParseMode.MARKDOWN)
     else:
