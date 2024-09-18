686bba4c4fb85b64b94eaeb875c8e8732cb78bf3
SACHINMUSIC <sachinxpapa@gmail.com>
2024-09-18 08:06:57 +0530

    [ ɴᴇxɪᴏ ᴍᴜsɪᴄ : 01 ]

--- NEXIOMUSIC/utils/inline/start.py
+++ NEXIOMUSIC/utils/inline/start.py
@@ -7,9 +7,7 @@ from NEXIOMUSIC import app
 def start_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
-            ),
+            InlineKeyboardButton(text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
     ]
@@ -19,16 +17,10 @@ def start_panel(_):
 def private_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
-            )
-        ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="MAIN_HELP_CP"),
-          InlineKeyboardButton(text=_["S_B_12"], callback_data=f"mbot_cb"),
+            InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true",)
         ],
         [
-            InlineKeyboardButton(text=_["S_B_13"], callback_data=f"bbot_cb"),
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="MAIN_HELP_CP"),
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
