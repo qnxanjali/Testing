cf8762af38241f93127f0fed0c47b4d9670488a5
SACHINMUSIC <sachinxpapa@gmail.com>
2024-09-19 17:12:42 +0530

    [ ɴᴇxɪᴏ ᴍᴜsɪᴄ : 01 ]

--- NEXIOMUSIC/plugins/bot/tool.py
+++ NEXIOMUSIC/plugins/bot/tool.py
@@ -2,14 +2,20 @@ from typing import Union
 from pyrogram import filters, types
 from pyrogram.types import InlineKeyboardMarkup, Message, InlineKeyboardButton
 from NEXIOMUSIC import app
-from NEXIOMUSIC.utils import help_pannell
-from NEXIOMUSIC.utils.inline.tool import help_back_markupp, private_help_panell
-from config import BANNED_USERS, START_IMG_URL, SUPPORT_CHANNEL
+from NEXIOMUSIC.utils import help_pannel
+from NEXIOMUSIC.utils.database import get_lang
+from NEXIOMUSIC.utils.decorators.language import LanguageStart, languageCB
+from NEXIOMUSIC.utils.inline.tool import help_back_markup, private_help_panel
+from config import BANNED_USERS, START_IMG_URL, SUPPORT_CHAT
 from strings import get_string, helpers
 
+#------------------------------------------------------------------------------------------------------------------------
+# MUSIC | MUSIC | MUSIC | MUSIC | MUSIC | MUSIC | MUSIC | MUSIC | MUSIC | MUSIC | MUSIC | MUSIC | MUSIC | MUSIC | MUSIC | 
+#------------------------------------------------------------------------------------------------------------------------
 
-@app.on_callback_query(filters.regex("tool_back_helper") & ~BANNED_USERS)
-async def helper_privatee(
+@app.on_message(filters.command(["thelp"]) & filters.private & ~BANNED_USERS)
+@app.on_callback_query(filters.regex("settings_back_helper") & ~BANNED_USERS)
+async def helper_private(
     client: app, update: Union[types.Message, types.CallbackQuery]
 ):
     is_callback = isinstance(update, types.CallbackQuery)
@@ -19,7 +25,9 @@ async def helper_privatee(
         except:
             pass
         chat_id = update.message.chat.id
-        keyboard = help_pannell(True)
+        language = await get_lang(chat_id)
+        _ = get_string(language)
+        keyboard = help_pannel(_, True)
         await update.edit_message_text(
             _["help_1"].format(SUPPORT_CHAT), reply_markup=keyboard
         )
@@ -28,26 +36,29 @@ async def helper_privatee(
             await update.delete()
         except:
             pass
-        update.chat.id
-        keyboard = help_pannell(_)
+        language = await get_lang(update.chat.id)
+        _ = get_string(language)
+        keyboard = help_pannel(_)
         await update.reply_photo(
             photo=START_IMG_URL,
             caption=_["help_1"].format(SUPPORT_CHAT),
             reply_markup=keyboard,
         )
-        
-        
+
+
 @app.on_message(filters.command(["thelp"]) & filters.group & ~BANNED_USERS)
-async def help_com_groupp(client, message: Message, _):
-    keyboard = private_help_panell(_)
-    await message.reply_text(_["help_2"], reply_markup=InlineKeyboardMarkupp(keyboard))
-    
-    
-@app.on_callback_query(filters.regex("thelp_callback") & ~BANNED_USERS)
-async def helper_cbb(client, CallbackQuery, _):
+@LanguageStart
+async def help_com_group(client, message: Message, _):
+    keyboard = private_help_panel(_)
+    await message.reply_text(_["help_2"], reply_markup=InlineKeyboardMarkup(keyboard))
+
+
+@app.on_callback_query(filters.regex("help_callback") & ~BANNED_USERS)
+@languageCB
+async def helper_cb(client, CallbackQuery, _):
     callback_data = CallbackQuery.data.strip()
     cb = callback_data.split(None, 1)[1]
-    keyboard = help_back_markupp(_)
+    keyboard = help_back_markup(_)
     if cb == "hhb1":
         await CallbackQuery.edit_message_text(helpers.HELP_01, reply_markup=keyboard)
     elif cb == "hhb2":
