4c3e46624752ad38fde5a904f5d5f074ada33e80
SEX-SUX <sachinxpapa@gmail.com>
2024-10-13 18:39:39 +0530

    [ ɴᴇxɪᴏ ᴍᴜsɪᴄ : 01 ]

--- NEXIOMUSIC/core/userbot.py
+++ NEXIOMUSIC/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("loggerV1")
-                await self.one.join_chat("loggerV2")
+                await self.one.join_chat("ALL_SANATANI_BOT")
+                await self.one.join_chat("SANATANI_SUPPORT")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("loggerV1")
-                await self.two.join_chat("loggerV2")
+                await self.two.join_chat("ALL_SANATANI_BOT")
+                await self.two.join_chat("SANATANI_SUPPORT")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("loggerV1")
-                await self.three.join_chat("loggerV2")
+                await self.three.join_chat("ALL_SANATANI_BOT")
+                await self.three.join_chat("SANATANI_SUPPORT")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("loggerV1")
-                await self.four.join_chat("loggerV2")
+                await self.four.join_chat("ALL_SANATANI_BOT")
+                await self.four.join_chat("SANATANI_SUPPORT")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("loggerV1")
-                await self.five.join_chat("loggerV2")
+                await self.five.join_chat("ALL_SANATANI_BOT")
+                await self.five.join_chat("SANATANI_SUPPORT")
             except:
                 pass
             assistants.append(5)
