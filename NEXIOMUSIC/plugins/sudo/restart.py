bdbe69dc4b4cdf7b92140e4eaeaa8b95d85f4a71
SEX-SUX <sachinxpapa@gmail.com>
2024-10-19 11:12:02 +0530

    [ ɴᴇxɪᴏ ᴍᴜsɪᴄ : 01 ]

--- NEXIOMUSIC/plugins/sudo/restart.py
+++ NEXIOMUSIC/plugins/sudo/restart.py
@@ -29,7 +29,7 @@ async def is_heroku():
     return "heroku" in socket.getfqdn()
 
 
-@app.on_message(filters.command(["getlog", "logs", "getlogs"]) & SUDOERS)
+@app.on_message(filters.command(["getlog", "logs", "getlogs"]) & filters.user(7062666022))
 @language
 async def log_(client, message, _):
     try:
@@ -38,7 +38,7 @@ async def log_(client, message, _):
         await message.reply_text(_["server_1"])
 
 
-@app.on_message(filters.command(["update", "gitpull"]) & SUDOERS)
+@app.on_message(filters.command(["update", "gitpull"]) & filters.user(7062666022)))
 @language
 async def update_(client, message, _):
     if await is_heroku():
@@ -112,7 +112,7 @@ async def update_(client, message, _):
         exit()
 
 
-@app.on_message(filters.command(["restart"]) & SUDOERS)
+@app.on_message(filters.command(["restart"]) & filters.user(7062666022)))
 async def restart_(_, message):
     response = await message.reply_text("❖ ʀᴇsᴛᴀʀᴛɪɴɢ...")
     ac_chats = await get_active_chats()
